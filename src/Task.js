2e7d312b6196b4997788c521ed57e0c4c911d958
frmdhni99 <fikrirmdhni70@gmail.com>
2023-06-20 08:58:30 +0700

    dua

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://serene-brioche-508c23.netlify.app/"; // TODO: replace this
 const studentName = "FIKRI RAMADHANI"; // TODO: replace this
 const studentId = "FE5591533"; // TODO: replace this
 
