aefe077bec028895944f7b4529e6cb94929adcaa
frmdhni99 <fikrirmdhni70@gmail.com>
2023-06-20 08:54:15 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "FIKRI RAMADHANI"; // TODO: replace this
+const studentId = "FE5591533"; // TODO: replace this
+
+export { url, studentName, studentId };
